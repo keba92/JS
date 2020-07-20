9639b99d42e06102678d185fbd6fe0fcf8b84a7b
keba92 <61150239+keba92@users.noreply.github.com>
2020-07-20 14:09:48 +0000

    new update

--- js/script.js
+++ js/script.js
@@ -127,3 +127,64 @@ function treygolnik(){
 }
 /////////////////////////////////////////////////////////////////////////////
 
+function getCount(str) {
+    var vowelsCount = 0;
+    let arr = str.split("");
+    for (let i=0; i<arr.length; i++){
+        if (arr[i] == "a"){
+            vowelsCount += 1;
+        }
+        else if (arr[i] == "e"){
+            vowelsCount += 1;
+        }
+        else if (arr[i] == "i"){
+            vowelsCount += 1;
+        }
+        else if (arr[i] == "o"){
+            vowelsCount += 1;
+        }
+        else if (arr[i] == "u"){
+            vowelsCount += 1;
+        }
+        else{
+        }
+    }   
+    return vowelsCount;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////
+  /**Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+    Examples input/output:
+
+    XO("ooxx") => true
+    XO("xooxx") => false
+    XO("ooxXm") => true
+    XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+    XO("zzoo") => false
+ */
+
+function XO(str) {
+    const arr = str.toLowerCase().split("");
+    console.log(arr);
+    let O = 0;
+    let X = 0;
+    for (let i=0; i<arr.length; i++){
+        if (arr[i] == "x"){
+            X+=1;
+        }
+        else if (arr[i] == "o"){
+            O+=1;
+        }
+        else{
+        }
+    }
+    if (X == O){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+XO("XoXesSOX");
