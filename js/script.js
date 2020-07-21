da3ad5ebafa212c15c766440a0f97f9be6282911
keba92 <61150239+keba92@users.noreply.github.com>
2020-07-21 17:01:40 +0000

    new example

--- js/script.js
+++ js/script.js
@@ -91,16 +91,16 @@ splitWord(" [SPAM] Y jhfsdf word sdfjksl", "spam");*/
 newArr("Alexey Kebets Mikhailovich");*/
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function showName () {
+/** function showName () {
     let fname = document.querySelector('.fname').value;
     let age = document.querySelector('.age').value;
     console.log(`Ваше имя: ${fname}`);
     console.log(`Ваш возраст: ${age}`);
-}
+} */
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function vvod () {
+/** function vvod () {
     let num = prompt("Введите число: ");
     if (num > 0){
         console.log("1");
@@ -114,20 +114,20 @@ function vvod () {
     else {
         console.log("Ошибка ввода");
     }
-}
+} */
 //////////////////////////////////////////////////////////////////////////////
 
-function treygolnik(){
+/** function treygolnik(){
     let stroka = document.querySelector(".stroka").value;
     let hash = ""
     for (let i=0; i < stroka; i++){
         hash = hash + "#";
         console.log(hash) 
     }
-}
+} */
 /////////////////////////////////////////////////////////////////////////////
 
-function getCount(str) {
+/** function getCount(str) {
     var vowelsCount = 0;
     let arr = str.split("");
     for (let i=0; i<arr.length; i++){
@@ -150,7 +150,7 @@ function getCount(str) {
         }
     }   
     return vowelsCount;
-  }
+  } */
 
   //////////////////////////////////////////////////////////////////////////////////
   /**Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
@@ -164,9 +164,8 @@ function getCount(str) {
     XO("zzoo") => false
  */
 
-function XO(str) {
+/** function XO(str) {
     const arr = str.toLowerCase().split("");
-    console.log(arr);
     let O = 0;
     let X = 0;
     for (let i=0; i<arr.length; i++){
@@ -185,6 +184,85 @@ function XO(str) {
     else {
         return false;
     }
-}
+} */
+////////////////////////////////////////////////////////////////////////////////////////////
+/**
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
-XO("XoXesSOX");
+It should remove all values from list a, which are present in list b.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+ */
+
+/** function arrayDiff(a, b) {
+  let c = a.filter(i=>b.indexOf(i) === -1);
+  return c;
+} */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/**You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+For example:
+
+Let's say you are given the array {1,2,3,4,3,2,1}: Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+Let's look at another one.
+You are given the array {1,100,50,-51,1,1}: Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+Last one:
+You are given the array {20,10,-80,10,10,15,35}
+At index 0 the left side is {}
+The right side is {10,-80,10,10,15,35}
+They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+Index 0 is the place where the left side and right side are equal.
+
+Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+
+Input:
+An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+
+Output:
+The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will return -1.
+
+Note:
+If you are given an array with multiple answers, return the lowest correct index. */
+
+/**
+function findEvenIndex(arr)
+{
+  let sumR = 0;
+  let sumL = 0;
+  let revArr = arr.reverse()
+  for (let i = 0; i<arr.length; i++){
+      sumL +=arr[i];
+
+  }
+  for (let j = 0; j<revArr.length; j++){
+    sumR +=revArr[j];
+    if(sumL == sumR){
+        console.log(sumL)
+    }
+  }
+} */
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+var maxSequence = function(arr){
+    let sum = 0;
+    let maxSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        if (sum > maxSum){ 
+            maxSum = sum;
+        }
+        if (sum < 0){
+            sum = 0;
+        }
+    }
+    return maxSum
+}
+  maxSequence([])
+*/
