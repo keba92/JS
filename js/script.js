941181eabd60ad10309e988fb5cc79c9413db70b
keba92 <61150239+keba92@users.noreply.github.com>
2020-07-24 20:12:10 +0000

    new

--- js/script.js
+++ js/script.js
@@ -326,3 +326,35 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
     return newArr.join(' ');
   }
   alphabetPosition("The sunset sets at twelve o' clock.");*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.*/
+
+/**function findUniq(arr) {
+    let newArr=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr.length >3){
+            if(arr[i] != arr[i+1] && arr[i] != arr[i-1]){
+                newArr.push(arr[i]);   
+            }
+        }
+        else{
+            if(arr[0] != arr[1] && arr[0] != arr[2]){
+                newArr.push(arr[0]);
+            }
+            else if (arr[1]!=arr[0] && arr[1]!=arr[2]){
+                newArr.push(arr[1]);
+            }
+        }
+    }
+    console.log(newArr[0]);
+  }
+  
+  findUniq([  1, 1, 2, 1, 1, 1 ])*/
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
