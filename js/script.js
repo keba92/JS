6e071cb922aec0895d126328f523a83eb647e8c8
keba92 <61150239+keba92@users.noreply.github.com>
2020-07-24 21:04:19 +0000

    new upd

--- js/script.js
+++ js/script.js
@@ -375,3 +375,42 @@ pigIt('Hello world !');     // elloHay orldway !*/
     return arr.join('');
   }
   pigIt('Pig latin is cool');*/
+
+  /**function pigIt(str){
+    let newStr = str.split(/(\W+)/);
+    let changed = newStr.map(input => {
+      if (!/\w/.test(input)) return input
+      return input.substring(1) + input.charAt(0) + "ay";
+    });
+    return changed.join("");
+  }
+
+  console.log(pigIt('Hello world !'))*/
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**Build Tower by the following given argument:
+number of floors (integer and always greater than 0).
+
+Tower block is represented as *
+
+Python: return a list;
+JavaScript: returns an Array;
+C#: returns a string[];
+PHP: returns an array;
+C++: returns a vector<string>;
+Haskell: returns a [String];
+Ruby: returns an Array;
+Lua: returns a Table;
+Have fun! */
+
+/**function towerBuilder(nFloors) {
+    let finalArray = [];
+    let numSpaces = 0;
+    // Start from the reverse.
+    for (let i = nFloors; i > 0; i--) {
+      finalArray.push(" ".repeat(numSpaces) + "*".repeat(i) + "*".repeat(i-1)  + " ".repeat(numSpaces));
+      numSpaces++;
+    }
+    return finalArray.reverse();
+  }
+  console.log(towerBuilder(5));*/
